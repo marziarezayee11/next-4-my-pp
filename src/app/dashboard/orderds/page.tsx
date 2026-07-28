fa08d9a3272baf6af81dc090bf92a164d700d5f6
marziarezayee11 <mandgarmarziaroya@gmail.com>
2026-07-28 16:46:39 +0430

    pages

--- src/app/dashboard/orderds/page.tsx
+++ src/app/dashboard/orderds/page.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <div className="h-screen w-full bg-linear-100 from-[#cdf1f1] to-[#aed3e1] via-[#c7ddeb]"><h1>
+       this is orderds page</h1></div>
   )
 }
 
--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <div className="h-screen w-full bg-linear-100 from-[#cdf1f1] to-[#aed3e1] via-[#c7ddeb]">
+    <h1>this is users page</h1>
+    
+    </div>
   )
 }
 
