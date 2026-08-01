b1e44f6ad03ed990209e8500ea69e73b969ff964
marziarezayee11 <mandgarmarziaroya@gmail.com>
2026-08-01 21:12:23 +0430

    style-users

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -3,36 +3,36 @@ import Image from 'next/image'
 function page() {
   return (
     <div className="h-screen w-full bg-linear-100 from-[#cdf1f1] to-[#aed3e1] via-[#c7ddeb] flex justify-center items-center gap-6 ">
-    <div className='flex justify-center items-center flex-col '>
+    <div className='flex justify-center items-center flex-col gap-2 '>
       <h1>hana</h1>
       
       <img src="/images/black2.jpg" alt="flower"
       className='h-40 w-40 rounded-full' />
-   <p>nnnnnnnnnnnnnn</p>v
+   <p>pardis</p>
     </div>
      <div className='flex justify-center items-center flex-col'>
       <h1>hana</h1>
       <img src="/images/black2.jpg" alt="flower"
       className='h-40 w-40 rounded-full' />
-   <p>nnnnnnnnnnnnnn</p>
+   <p>pardis</p>
     </div>
      <div className='flex justify-center items-center flex-col'>
       <h1>hana</h1>
       <img src="/images/black2.jpg" alt="flower"
       className='h-40 w-40 rounded-full' />
-   <p>nnnnnnnnnnnnnn</p>
+   <p>pardis</p>
     </div>
      <div className='flex justify-center items-center flex-col'>
       <h1>hana</h1>
       <img src="/images/black2.jpg" alt="flower"
       className='h-40 w-40 rounded-full' />
-   <p>nnnnnnnnnnnnnn</p>
+   <p>pardis</p>
     </div>
      <div className='flex justify-center items-center flex-col'>
       <h1>hana</h1>
       <img src="/images/black2.jpg" alt="flower"
       className='h-40 w-40 rounded-full' />
-   <p>nnnnnnnnnnnnnn</p>
+   <p>pardis</p>
     </div>
     
     </div>
