ed7630029973a6a5e0e54bf1219ec31ef54cbedc
marziarezayee11 <mandgarmarziaroya@gmail.com>
2026-08-06 09:52:37 +0430

    CON-REN-routes

--- src/app/menu/layout.tsx
+++ src/app/menu/layout.tsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import "../(all)/globals.css"
-function layout({music,children,videos,photos}:{
-    music:React.ReactNode,
-    children:React.ReactNode,
-    videos:React.ReactNode,
-    photos:React.ReactNode,
+import React from 'react';
+import '../(all)/globals.css';
+
+function Layout({
+  music,
+  children,
+  videos,
+  photos,
+  login,
+}: {
+  music: React.ReactNode;
+  children: React.ReactNode;
+  videos: React.ReactNode;
+  photos: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
-    <html>
+  const isloggedin = false;
+
+  if (isloggedin) {
+    return (
+      <html>
         <body>
-            
-        
-   
-    <h1 className='py-2 text-center text-2xl font-bold'>Menu</h1>
-     <div className='h-screen w-full grid grid-cols-2 gap-4 mx-auto p-4'>
-    <div>{children}</div>
-    <div>{music}</div>
-    <div>{videos}</div>
-    <div>{photos}</div>
-    </div>
-    </body>
-    </html>
-  )
+          <div className="min-h-screen w-full bg-white">
+            <h1 className="py-2 text-center text-2xl font-bold">منو</h1>
+            <div className="h-screen w-full grid grid-cols-2 gap-4 mx-auto p-4">
+              <div>{children}</div>
+              <div>{music}</div>
+              <div>{videos}</div>
+              <div>{photos}</div>
+            </div>
+          </div>
+        </body>
+      </html>
+    );
+  } else {
+    return (
+      <html>
+        <body>
+          <div className="min-h-screen w-full flex items-center justify-center bg-gray-100">
+            {login}
+          </div>
+        </body>
+      </html>
+    );
+  }
 }
 
-export default layout
+export default Layout;
+;
+
