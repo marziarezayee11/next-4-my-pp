3d8418173bb12ae9898f1205a2bd5161f70a01e8
marziarezayee11 <mandgarmarziaroya@gmail.com>
2026-08-08 21:41:23 +0430

    vid

---
+++ src/app/(all)/component/vid.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Did() {
+
+  return (
+    <div className='flex justify-center items-center'>
+         <h1 className=' text-center font-bold'>Videos</h1>
+        <div className='flex justify-center items-center gap-3'>
+          <div>
+            <video
+            className='h-60'
+            controls width={260}>
+              <source src="/videos/de1.mp4"
+              type='video/mp4'/>
+              error
+           </video>
+          </div>
+             <div>
+            <video
+            className='h-44'
+            controls width={250}>
+              <source src="/videos/de3.mp4"
+              type='video/mp4'/>
+              error
+           </video>
+          </div>
+             <div>
+            <video
+            className='h-56'
+            controls width={250}>
+              <source src="/videos/de2.mp4"
+              type='video/mp4'/>
+              error
+           </video>
+          </div>
+          
+        </div>
+    </div>
+  )
+}
+
+export default Did
--- src/app/(all)/dashboard/products/page.tsx
+++ src/app/(all)/dashboard/products/page.tsx
@@ -2,29 +2,63 @@ import React from 'react'
 import Image from 'next/image'
 function page() {
   return (
-    <div className="h-screen w-full bg-linear-100 from-[#cdf1f1] to-[#aed3e1] via-[#c7ddeb] flex justify-center items-center gap-5">
-   
-      <div>
-      <div><Image
+    <div className="h-full w-full bg-linear-100 from-[#cdf1f1] to-[#aed3e1] via-[#c7ddeb] gap-5">
+   <h1 className='text-center font-extrabold mt-28 mb-4 text-2xl'> Photos</h1>
+      <div className=' flex justify-center items-center gap-5'>
+      <Image
       className='rounded-3xl' src="/images/red3.jpg" alt='foot' 
       height={250}
-      width={250}/></div>
-        </div>
-          <div>
-      <div><Image
+      width={250}/>
+      
+          
+      <Image
        className='rounded-3xl'
       src="/images/red4.jpg" alt='foot' 
       height={300}
-      width={300}/></div>
-        </div>
-          <div>
-      <div><Image
+      width={300}/>
+        
+      <Image
        className='rounded-3xl'
       src="/images/red5.jpg" alt='foot' 
       height={250}
-      width={250}/></div>
+      width={250}/>
+      </div>
+        
+           <div className='mt-10 mb-7'>
+         <h1 className=' text-center font-bold text-2xl'>Videos</h1>
+        <div className='flex justify-center items-center gap-3'>
+          <div>
+            <video
+            className='h-96'
+            controls width={300}>
+              <source src="/videos/de1.mp4"
+              type='video/mp4'/>
+              error
+           </video>
+          </div>
+             <div>
+            <video
+            className='h-80 '
+            controls width={300}>
+              <source src="/videos/de3.mp4"
+              type='video/mp4'/>
+              error
+           </video>
+          </div>
+             <div>
+            <video
+            className='h-96'
+            controls width={300}>
+              <source src="/videos/de2.mp4"
+              type='video/mp4'/>
+              error
+           </video>
+          </div>
+          
         </div>
+    </div>
         </div>
+        
   )
 }
 
